51f16bc06de5a6ddeacacd5d9de1d61021b815ba
Mat Chop <mat---@hotmail.fr>
2022-01-31 13:38:56 +0100

    onglets bugfix + ajout lien newsletter top

--- docs/js/app.js
+++ docs/js/app.js
@@ -1,7 +1,3 @@
-//  Position du menu vertical visible en vue mobile
-const heroNavbar = document.getElementById("navbar-small")
-let botCoord = heroNavbar.getBoundingClientRect().bottom + window.scrollY - 76
-
 //  Coordonnées Y  pour trigger le changement d'onglet actif
 let chap1 = document.getElementById("overview").getBoundingClientRect().top + window.scrollY - 120
 let chap2 = document.getElementById("screenshots").getBoundingClientRect().top + window.scrollY - 140
@@ -12,32 +8,31 @@ let chap5 = document.getElementById("sponsors").getBoundingClientRect().top + wi
 let screenWidth = screen.width
 window.addEventListener("resize", () => {
   screenWidth = screen.width
+  chap1 = document.getElementById("overview").getBoundingClientRect().top + window.scrollY - 120
+  chap2 = document.getElementById("screenshots").getBoundingClientRect().top + window.scrollY - 140
+  chap3 = document.getElementById("roadmap").getBoundingClientRect().top + window.scrollY - 120
+  chap4 = document.getElementById("contact").getBoundingClientRect().top + window.scrollY - 120
+  chap5 = document.getElementById("sponsors").getBoundingClientRect().top + window.scrollY - 130
 })
 
 // Décalage horizontal pour centrer l'onglet actif dans la navbar sticky
 let xOffset = (screen.width - 180) / 2
 
-//  Affiche/masque menu vertical + change l'onglet actif
+//  Onglet actif dans la navbar
 window.addEventListener("scroll", () => {
-    if (window.scrollY < botCoord) {
-      heroNavbar.style.opacity = "1"
-      heroNavbar.style.pointerEvents = "initial"
-      document.querySelector(".active-link")?.classList.remove("active-link")
-    } else {
-      heroNavbar.style.opacity = "0"
-      heroNavbar.style.pointerEvents = "none"
-    }
-    if (window.scrollY > chap1 && window.scrollY < chap2) {
-      toggleActiveLink("chap1")
-    } else if (window.scrollY > chap2 && window.scrollY < chap3) {
-      toggleActiveLink("chap2")
-    } else if (window.scrollY > chap3 && window.scrollY < chap4) {
-      toggleActiveLink("chap3")
-    } else if (window.scrollY > chap4 && window.scrollY < chap5) {
-      toggleActiveLink("chap4")
-    } else if (window.scrollY > chap5) {
-      toggleActiveLink("chap5")
-    }
+  if (window.scrollY < chap1) {
+    document.querySelector(".active-link")?.classList.remove("active-link")
+  } else if (window.scrollY > chap1 && window.scrollY < chap2) {
+    toggleActiveLink("chap1")
+  } else if (window.scrollY > chap2 && window.scrollY < chap3) {
+    toggleActiveLink("chap2")
+  } else if (window.scrollY > chap3 && window.scrollY < chap4) {
+    toggleActiveLink("chap3")
+  } else if (window.scrollY > chap4 && window.scrollY < chap5) {
+    toggleActiveLink("chap4")
+  } else if (window.scrollY > chap5) {
+    toggleActiveLink("chap5")
+  }
 })
 
 function toggleActiveLink(x) {
